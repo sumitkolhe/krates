7a3bff2a66387492313080ec27a3c56b6296fdd7
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-09-30 20:49:09 +0530

    :construction: Added initial Website frontend

---
+++ web/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style >
+* {
+  font-family: "Montserrat", sans-serif;
+}
+</style>
