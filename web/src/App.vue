d242a09341c28ea41c5687713bf3b2c7fd6f0c84
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2020-10-03 08:30:08 +0530

    :lipstick: Updated Web

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,6 +3,16 @@
     <v-main>
       <Home />
     </v-main>
+    <v-footer app padless color="footerColor">
+      <v-col align="center" cols="12">
+        <h4 class="font-weight-medium">
+          Made with ❤️ by
+          <a class="text-decoration" href="https://sumit.codes" target="_blank"
+            >Sumit</a
+          >
+        </h4>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
@@ -25,5 +35,14 @@ export default {
 <style >
 * {
   font-family: "Montserrat", sans-serif;
+  overflow-x: hidden;
+}
+
+a,
+a:active,
+a:hover,
+a:focus {
+  text-decoration: none;
+  color: #000;
 }
 </style>
--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -10,10 +10,12 @@ export default new Vuetify({
       light: {
         textColor: "#fff",
         btnColor: "#e3e3e3",
+        footerColor:"#fff"
       },
       dark: {
         textColor: "#000",
         btnColor: "#121212",
+        footerColor:"#121212"
       },
     },
   },
