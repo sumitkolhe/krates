557ef18a32e62706cad2c9b091203a2a77344ddc
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-09-21 20:17:00 +0530

    :pencil2: Added comments

--- src/services/bucket.service.ts
+++ src/services/bucket.service.ts
@@ -3,13 +3,16 @@ import { BucketModel } from '@src/models/bucket.model'
 import { sanitizeResponse } from '@src/utils/sanitizeResponse'
 
 export class BucketService {
+  // Get all data from a bucket
   static getBucketData = async (bucketId: string): Promise<Bucket[] | Bucket> => {
     const bucketData = await BucketModel.find({ bucketId })
     return bucketData
   }
 
+  // Insert data into a bucket
   static setBucketData = async (bucketId: string, data: Bucket): Promise<Bucket[] | Bucket> => {
-    const createRecord = async (dataObject: Pick<Bucket, 'data'>): Promise<Bucket> => {
+    // Insert all objects in DB
+    const createRecord = async (dataObject: Bucket): Promise<Bucket> => {
       const newRecord = new BucketModel({
         bucketId,
         data: dataObject,
@@ -18,17 +21,18 @@ export class BucketService {
       return sanitizeResponse(await newRecord.save())
     }
 
+    // Check if data is an Array or a object
     if (Array.isArray(data)) {
       const createRecordPromise = data.map(createRecord)
-      const responses = await Promise.all(createRecordPromise)
-      return responses
+      const records = await Promise.all(createRecordPromise)
+      return records
     }
 
-    const newBucket = new BucketModel({
+    const newRecord = new BucketModel({
       bucketId,
       data,
     })
 
-    return sanitizeResponse(await newBucket.save())
+    return sanitizeResponse(await newRecord.save())
   }
 }
--- src/services/collection.service.ts
+++ src/services/collection.service.ts
@@ -3,16 +3,19 @@ import { BucketModel } from '@src/models/bucket.model'
 import { sanitizeResponse } from '@src/utils/sanitizeResponse'
 
 export class CollectionService {
+  // Get data for a collection
   static getCollectionData = async (bucketId: string, collectionId: string): Promise<Bucket[] | Bucket> => {
     const collectionData = await BucketModel.find({ bucketId, collectionId })
     return collectionData
   }
 
+  // Insert data into a collection
   static setCollectionData = async (
     bucketId: string,
     collectionId: string,
     data: Bucket
   ): Promise<Bucket[] | Bucket> => {
+    // Insert all objects in DB
     const createRecord = async (dataObject: Bucket): Promise<Bucket> => {
       const newRecord = new BucketModel({
         bucketId,
@@ -23,10 +26,11 @@ export class CollectionService {
       return sanitizeResponse(await newRecord.save())
     }
 
+    // Check if data object is an Array or object
     if (Array.isArray(data)) {
       const createRecordPromise = data.map(createRecord)
-      const responses = await Promise.all(createRecordPromise)
-      return responses
+      const records = await Promise.all(createRecordPromise)
+      return records
     }
 
     const newBucket = new BucketModel({
