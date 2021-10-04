6bd40577cae31d9b3cdaea6f0bc50e11fbe2b647
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-10-05 00:06:56 +0530

    :rocket: Add server redirect

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,8 @@ import { baseRouter } from '@src/routes/base.routes'
 
 export const parentRouter: Router = express.Router()
 
+parentRouter.get('/', (req, res) => {
+  res.redirect(301, 'https://app.detabase.me')
+})
 parentRouter.use('/api', baseRouter)
 parentRouter.use('/meta', baseRouter)
