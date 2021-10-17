d56d36394187f0c385fc5d9a46d1a80788639cf1
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-10-17 13:06:54 +0530

    :bug: logger fix

--- src/middleware/morgan.ts
+++ src/middleware/morgan.ts
@@ -1,15 +1,16 @@
 import morgan, { StreamOptions } from 'morgan'
 import { Logger } from '@src/utils/logger'
-//  import { isDev } from '@src/utils/isDev'
+import { isDev } from '@src/utils/isDev'
 
 const stream: StreamOptions = {
   write: (message) => Logger.http(message),
 }
 
-// const skip = () => {
-//   return !isDev()
-// }
+const skip = () => {
+  return isDev()
+}
 
 export const morganMiddleware = morgan(':method :url :status :res[content-length] - :response-time ms', {
   stream,
+  skip,
 })
