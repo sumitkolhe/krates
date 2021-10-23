7ac47c88175e2f671fb54cd8fd3115fd4a8231af
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-10-23 19:33:07 +0530

    :bug: Fixed validation messages

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -4,8 +4,12 @@ import { celebrate, Segments } from 'celebrate'
 export const validators = {
   storage: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
-      krateId: Joi.string().trim().length(20).alphanum().required(),
-      collectionId: Joi.string().trim().min(1).max(16).optional(),
+      krateId: Joi.string().trim().length(20).alphanum().required().messages({
+        'string.empty': 'krate ID name cannot be empty',
+        'string.length': 'krate ID should be 20 characters long',
+        'string.alphanum': 'krate ID should alpha-numeric',
+      }),
+      collectionId: Joi.string().trim().min(1).max(16).message('invalid collection id').optional(),
     }),
     [Segments.BODY]: Joi.alternatives(Joi.object().min(1), Joi.array().min(1)),
   }),
