5493f70dd52d11a161b312feb60dcf345cbaa762
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-11-07 10:51:58 +0530

    :pencil2: Updated rate limit points

--- src/config/global.ts
+++ src/config/global.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv-safe'
 dotenv.config({ allowEmptyValues: true })
 
 export const globalConfig = {
-  rateLimitPoints: process.env.RATE_LIMIT_POINTS ? Number(process.env.RATE_LIMIT_POINTS) : 6,
-  rateLimitDuration: process.env.RATE_LIMIT_DURATION ? Number(process.env.RATE_LIMIT_DURATION) : 10, // in seconds
+  rateLimitPoints: process.env.RATE_LIMIT_POINTS ? Number(process.env.RATE_LIMIT_POINTS) : 20,
+  rateLimitDuration: process.env.RATE_LIMIT_DURATION ? Number(process.env.RATE_LIMIT_DURATION) : 60, // in seconds
   maxPayloadSize: process.env.PAYLOAD_SIZE_LIMIT ? Number(process.env.PAYLOAD_SIZE_LIMIT) * 1000 : 100000, // in Kb
 }
