97690a3cb23692d3f6d87f1c7b088dc9c46b9bc5
Hugh Lilly <hughlilly@users.noreply.github.com>
2022-09-17 14:15:39 +1200

    hyphenate "Backend-Agnostic" (it's adjectival here)

--- dashboard/components/home/features.vue
+++ dashboard/components/home/features.vue
@@ -1,22 +1,18 @@
 <template>
   <section class="relative">
-    <div
-      class="
+    <div class="
         absolute
         inset-0
         lg:mt-0
         bg-accent1
         border-accent2 border-t border-b
         pointer-events-none
-      "
-      aria-hidden="true"
-    ></div>
+      " aria-hidden="true"></div>
 
     <div class="relative max-w-5xl mx-auto px-6">
       <h2 class="text-4xl font-black mb-2 text-center pt-20">Krates is</h2>
       <div class="py-12 md:pb-24">
-        <div
-          class="
+        <div class="
             grid
             gap-8
             md:grid-cols-2
@@ -25,14 +21,9 @@
             md:max-w-2xl
             lg:max-w-none
             mx-auto
-          "
-        >
-          <zi-card
-            hoverable
-            class="flex flex-col items-center"
-            v-for="(feature, index) in features"
-            :key="feature.title"
-          >
+          ">
+          <zi-card hoverable class="flex flex-col items-center" v-for="(feature, index) in features"
+                   :key="feature.title">
             <template v-if="index === 0">
               <circle-icon class="h-16 w-16" />
             </template>
@@ -83,7 +74,7 @@ export default Vue.extend({
           description: 'Saving your data is just a HTTP request away.',
         },
         {
-          title: 'Backend Agnostic',
+          title: 'Backend-Agnostic',
           description:
             'Use krates with a backend server or directly from the browser.',
         },
